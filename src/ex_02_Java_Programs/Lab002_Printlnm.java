8072161f8e8330fd7a7fc90ee566ee82ea078c96
supriyakawale1990 <125757547+supriyakawale1990@users.noreply.github.com>
2025-01-08 23:55:41 +0530

    jan05

---
+++ src/ex_02_Java_Programs/Lab002_Printlnm.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Programs;
+
+public class Lab002_Printlnm
+{
+    public static void main(String[] args)
+    {
+        String name="Supriya";
+        int a=90;
+        boolean b = true;
+        char sec='A';
+        System.out.println("User Name:"+ name);
+        System.out.println("marks:"+ a);
+        System.out.println("Boolean value:"+ b);
+        System.out.println("Section:"+ sec);
+    }
+
+}
