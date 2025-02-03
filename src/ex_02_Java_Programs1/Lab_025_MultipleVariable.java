a8df4bd1f93144d345f55b34d537be230c5576a2
supriyakawale1990 <125757547+supriyakawale1990@users.noreply.github.com>
2025-02-03 22:03:21 +0530

    Merge remote-tracking branch 'github/master'

---
+++ src/ex_02_Java_Programs1/Lab_025_MultipleVariable.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Programs1;
+
+public class Lab_025_MultipleVariable
+{
+    public static void main(String[] args) {
+        int a = 45, b=40, c= 60, d=50;
+       // int d=44;
+        System.out.println(d);
+    }
+}
